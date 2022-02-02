ce3d99e26466cee3e38d6da9e16cbf62be36234c
Constantine Nazarenko <const@nazarenko.es>
2022-02-02 18:43:37 +0200

    fix webpack prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,31 +1,31 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: "/",
-        filename: '[name].[hash].js'
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.js',
+        library: 'CV',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        umdNamedDefine: true,
     },
     optimization: {
-        minimizer: [
-        ]
+        minimize: true,
     },
     plugins: [
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: "[name].[hash].css",
-            chunkFilename: "[id].[hash].css"
-        })
+            filename: "index.css"
+        }),
     ],
     module: {
         rules: [
-            { test: /\.(sa|sc|c)ss$/, exclude: /node_modules/,
+            { test: /\.(sa|sc|c)ss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
