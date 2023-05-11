0ab61ced4a7e05d67a55151ddd5b16d04d3aab1b
Constantine Nazarenko <const@nazarenko.es>
2023-05-11 19:42:44 +0300

    optimize

--- src/index.tsx
+++ src/index.tsx
@@ -39,7 +39,7 @@ function onRender(
 ) {
     // Aggregate or log render timings...
     console.log(id, phase, actualDuration, baseDuration, startTime, commitTime);
-}
+}``
 
 root.render(
     <Suspense fallback={<Index />}>
