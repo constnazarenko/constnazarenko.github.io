e7aca5545a1f3e13d179cb7ec961ad4bd5077145
Kostiantyn Nazarenko <const@nazarenko.es>
2025-04-07 20:20:00 +0200

    update contacts; fix contacts styles; add copy functionality;

--- src/app/Contacts/index.tsx
+++ src/app/Contacts/index.tsx
@@ -1,17 +1,19 @@
-import { IconProp, library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { faMapMarkerAlt, faPhone } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { Fragment } from "react";
-import classnames from "classnames";
+import { IconProp, library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classnames from 'classnames';
+import copy from 'copy-to-clipboard';
+import React, { Fragment } from 'react';
 
-import "./styles.scss";
-import contacts from "../../api/contacts.json";
+import contacts from '../../api/contacts.json';
+import './styles.scss';
 
 library.add(fab, far, faMapMarkerAlt, faPhone);
 
 interface ContactItem {
+  entity: string;
   title: string;
   icon: IconProp;
   alt?: string;
@@ -19,25 +21,49 @@ interface ContactItem {
   nonprintable?: boolean;
 }
 interface ContactsProps {
+  name: string;
+  title: string;
   isPrintable: boolean;
 }
 
-const Contacts: React.FC<ContactsProps> = ({ isPrintable }) => (
-  <div className={classnames({ contacts: true, "is-printable": isPrintable })}>
+const copiableContants = contacts
+  .filter((contact) => !contact.nonprintable)
+  .map((contact) => `${contact.entity}: ${contact.title}`)
+  .join('\n');
+
+const Contacts: React.FC<ContactsProps> = ({ name, title, isPrintable }) => (
+  <div className={classnames({ contacts: true, 'is-printable': isPrintable })}>
+    {!isPrintable && (
+      <div className="copy-contacts">
+        <a
+          onClick={() => {
+            copy(`${name}\n${title}\n\n${copiableContants}`);
+          }}
+        >
+          Copy contacts data
+        </a>
+      </div>
+    )}
+
     <dl>
       {(contacts as ContactItem[]).map((contact) => {
         if (isPrintable && contact.nonprintable && !contact.alt) {
           return <Fragment key={contact.title} />;
         }
-        const caption =
-          isPrintable && contact.alt ? contact.alt : contact.title;
+        const caption = isPrintable && contact.alt ? contact.alt : contact.title;
         return (
           <Fragment key={contact.title}>
             <dt>
               <FontAwesomeIcon icon={contact.icon} />
             </dt>
             <dd>
-              {contact.href ? <a href={contact.href} target="_blank">{caption}</a> : caption}
+              {contact.href ? (
+                <a href={contact.href} target="_blank" rel="noreferrer">
+                  {caption}
+                </a>
+              ) : (
+                caption
+              )}
             </dd>
           </Fragment>
         );
